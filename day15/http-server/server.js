eba1072723b4f7ff54e7c3e17adcb944168af97c
sooooooool <awesomena89@gmail.com>
2024-07-23 17:15:41 +0900

    day15

---
+++ day15/http-server/server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+
+    const path = url.parse(req.url, true).pathname
+
+    if(path == '/user') {
+
+    } else if(path == '/feed') {    
+    } else if(path == '/community') {    
+    } else if(path == '/order') {    
+    } else if(path == '/mypage') {    
+    } else if(path == '/feed') {    
+    }
+
+    res.setHeader("Content-Type", "text/html")
+    res.end("OK")
+});
+
+server.listen("3000", () => console.log("OK 서버 시작!"));
